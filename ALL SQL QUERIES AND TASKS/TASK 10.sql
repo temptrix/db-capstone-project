6ce3f4cb82ab729e3f46a38ceb2c5ba196a6e76a
temptrix <uvieomimi@gmail.com>
2024-05-20 21:30:23 +0100

    Adding the AddBooking stored procedure for tha bookingg system

--- ALL SQL QUERIES AND TASKS/TASK 10.sql
+++ ALL SQL QUERIES AND TASKS/TASK 10.sql
@@ -1,9 +1,15 @@
 
 DELIMITER //
-CREATE PROCEDURE AddBooking (IN BookingID INT, IN CustomerID INT, IN BookDate DATE, IN TableNo INT)
+CREATE PROCEDURE AddBooking (
+    IN BookingID INT, 
+    IN CustomerID INT, 
+    IN BookDate DATE, 
+    IN TableNo INT
+    )
 BEGIN
 	INSERT INTO bookings (BookingID, CustomerID, BookingDate, TableNo)
     VALUES (BookingID, CustomerID, BookingDate, TableNo);
     SELECT "New booking Added" AS Confirmation;
 END//
+
 DELIMITER ;
